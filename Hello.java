a003e96da7abfb0f79502827136b7c7f84d9d006
MerricsUniqueUsername <45721586+MerricsUniqueUsername@users.noreply.github.com>
2026-02-24 22:12:10 -0500

    Null pointer

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        String text = null;
+        System.out.println(text.length()); // This causes NullPointerException
+    }
+}
--- WrongName.java
+++
@@ -1,5 +0,0 @@
-public class WrongName {
-    public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
-    }
-}
